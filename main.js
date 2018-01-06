d305f5f063d193b367333f2cf105c720445e6f52
Flamaster <szumlanskim@gmail.com>
2018-01-06 21:39:13 +0100

    Added hotspots beta

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
-window.addEventListener('load', onVrViewLoad)
-
 var vrView;
 
 var scenes = {
 	B5: {
 		image: 'img/B5.jpg',
-		is_stereo: true
+		is_stereo: false,
+		hotspots: {
+      B4: {
+        pitch: 0,
+        yaw: 110,
+        radius: 0.05,
+        distance: 1
+      }    
+    }
+},
+	B4: {
+			image: 'img/B4.jpg',
+			is_stereo: false,
+			hotspots: {
+	      B5: {
+	        pitch: 0,
+	        yaw: 110,
+	        radius: 0.05,
+	        distance: 1
+	      }    
+	    }
 	}
-}
+
+	}
+	
+window.addEventListener('load', onVrViewLoad)
+
 function onVrViewLoad() {
     vrView = new VRView.Player('#vrview', {
   	width: '100%',
     height: 480,
     image: 'img/B5.jpg',
-    is_stereo: true
-    
+    is_stereo: true  
   });
 
 
   vrView.on('ready', onVRViewReady);
   vrView.on('modechange', onModeChange);
+  vrView.on('click', onHotspotClick);
   vrView.on('getposition', onGetPosition);
   vrView.on('error', onVRViewError);
 }
@@ -27,14 +49,34 @@ function onVrViewLoad() {
 function loadScene(id) {
   console.log('loadScene', id);
 
-  // Set the image
   vrView.setContent({
     image: scenes[id].image,
-    preview: scenes[id].preview//,
-   // is_stereo: true,
-   //is_autopan_off: true
+    preview: scenes[id].preview
   });
 
+  // Add all the hotspots for the scene
+  var newScene = scenes[id];
+  var sceneHotspots = Object.keys(newScene.hotspots);
+  for (var i = 0; i < sceneHotspots.length; i++) {
+    var hotspotKey = sceneHotspots[i];
+    var hotspot = newScene.hotspots[hotspotKey];
+
+    vrView.addHotspot(hotspotKey, {
+      pitch: hotspot.pitch,
+      yaw: hotspot.yaw,
+      radius: hotspot.radius,
+      distance: hotspot.distance
+    });
+  }
+
+}
+
+function onHotspotClick(e) {
+  vrView.getPosition()
+  console.log('onHotspotClick', e.id);
+  if (e.id) {
+    loadScene(e.id);
+  }
 }
 
 function onVRViewReady(e) {
